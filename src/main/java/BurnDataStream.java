52e1e7cc039a1bba1372ec4002f99538e2e9d974
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-04-07 08:22:47 -0400

    last change

--- src/main/java/BurnDataStream.java
+++ src/main/java/BurnDataStream.java
@@ -3,10 +3,12 @@ public class BurnDataStream implements BurnStream {
     // change them to see if you can get the lander to make a soft landing.
     // burns are between 0 and 200. This burn array usually crashes.
 
-    int burnArray[] = {100, 100, 200, 200, 100, 100, 0, 0, 200, 100, 100, 0, 0, 0, 0};
-    int burnIdx = -1;
+    int burnArray[];
+    int burnIdx = 0;
 
-    public BurnDataStream() { }
+    public BurnDataStream() {
+        burnArray= new int[0];
+    }
     public BurnDataStream(int[] burns) {
         this.burnArray = burns;
     }
