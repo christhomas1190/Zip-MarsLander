f55691d0c23ed0b52e3d9e1058a159781592c24e
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-04-07 08:10:09 -0400

    changed burn input stream

--- src/main/java/BurnInputStream.java
+++ src/main/java/BurnInputStream.java
@@ -9,15 +9,15 @@ public class BurnInputStream implements BurnStream {
         while (true) {
         System.out.println("Enter. burn amount 0-200");
         String[] tokens = scanner.nextLine().split(" ");
-        //I think here is where I would handle anything outside of 0-200?
+        //I changed code so that only numbers from 0-200 can be used
         if (tokens.length > 0) {
-                try {
                     int burn = Integer.parseInt(tokens[0]);
-                    return burn;
-                } catch (NumberFormatException e) {
-                    System.err.println("Must Enter a Number (0-200)");
+                    //Burn number must be or be between 0-200
+                    if(burn>=0 && burn<=200){
+                        return burn;
+                    }
                 }
             }
         }
     }
-}
+
